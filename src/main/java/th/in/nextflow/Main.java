b473f9f3a8fc28e1920b8a67a8bcab1fd3585213
Teerasej <teerasej@gmail.com>
2024-10-08 15:01:11 +0700

    using the implemented class in code

--- src/main/java/th/in/nextflow/Main.java
+++ src/main/java/th/in/nextflow/Main.java
@@ -3,5 +3,9 @@ package th.in.nextflow;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        // Using the Calculator class
+        Calculator calculator = new Calculator();
+        System.out.println("2 + 3 = " + calculator.add(2, 3));
     }
 }
